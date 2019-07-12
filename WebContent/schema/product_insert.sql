5e2f71109c786e70b020490385ff196b515c525f
정윤 <@DESKTOP-FFGJRKD>
2019-07-12 15:26:49 +0900

    pm 03:26 장바구니 스타일적용 완료

---
+++ WebContent/schema/product_insert.sql
@@ -0,0 +1,49 @@
+product table
+
+insert into product VALUES('a01','단팥빵','단팥빵.jpg','1500','단팥, 밀가루, 우유, 계란으로 만들어진 빵');
+
+insert into product VALUES('a02','머핀','머핀.jpg','2000','부드럽고 촉촉한 머핀~~');
+
+insert into product VALUES('a03','꽈배기빵','꽈배기빵.jpg','1500','추억의 꽈배기빵!');
+
+insert into product VALUES('a04','소보루빵','소보루빵.jpg','1000','고소한 소보루');
+
+insert into product VALUES('a05','베이글','베이글.jpg','2000','아침 대용 베이글');
+
+insert into product VALUES('a06','식빵',   '식빵.jpg','2000','먹기 편한 식빵');
+
+insert into product VALUES('a07','바게트빵','바게트빵.jpg','2500','아침 식대 대용으로 바게트');
+
+insert into product VALUES('a08','블루베리베이글','블루베리베이글.jpg','2300','블루베리가 들어가 상큼한 베이글');
+
+insert into product VALUES('a09','샌드위치','샌드위치.jpg','1800','아침식사로 딱!');
+
+insert into product VALUES('a10','새우감자빵','새우감자빵.jpg','1600','빵속 감자와 새우가 가득!!');
+
+insert into product VALUES('a11','소라빵','소라빵.jpg','1200','초코 가득 소라빵 존맛탱~');
+
+insert into product VALUES('a12','소세지피자빵','소세지피자빵.jpg','2700','한끼 식사로 딱~!');
+
+insert into product VALUES('a13','아몬드치즈빵','아몬드치즈빵.jpg','1900','고소한 아몬드와 치즈의 조화');
+
+insert into product VALUES('a14','유자크림빵','유자크림빵.jpg','2000','유자크림 이 듬뿌들어간 빵');
+
+insert into product VALUES('a15','잡곡크림빵','잡곡크림빵.jpg','2000','잡곡이 들어가 건강한 빵');
+
+insert into product VALUES('a16','찹쌀도넛','찹쌀도넛.jpg','500',   '쫀득한 찹쌀이 가득');
+
+insert into product VALUES('a17','초코도너츠','초코도너츠.jpg','1500','도넛에 초코가 들어간 도넛');
+
+insert into product VALUES('a18','초코마카롱','초코마카롱.jpg','700','진한 초코가 많음!!');
+
+insert into product VALUES('a19','초코칩트위스트','초코칩트위스트.jpg','2300','달콤한 초코칩이 들어가 있음!!');
+
+insert into product VALUES('a20','초코페스츄리','초코페스츄리.jpg','2700','초코와 페스츄리의 환상조화~');
+
+insert into product VALUES('a21','초코호두브리오쉬','초코호두브리오쉬.jpg','3000','달콤초코와 고소한 호두가 한 가득!!');
+
+insert into product VALUES('a22','커피번','커피번.jpg','2500','커피향이 가득한 커피번~~!');
+
+insert into product VALUES('a23','크로와상','크로와상.jpg','1600','부드럽고 고소한 크로와상!!');
+
+insert into product VALUES('a24','티라미슈빵','티라미슈빵.jpg', '1700','진한 티라미슈 존맛~');
